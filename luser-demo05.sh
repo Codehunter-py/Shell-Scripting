9bd3f6ba9fe1484d795dd441e80185ca2234d92a
Codehunter-py <ibrahim.musayev2021@outlook.com>
2021-08-02 20:25:40 +0200

    RGenerating a list of random passwords.

---
+++ luser-demo05.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script generates a list of random passwords.
+
+# A random number as a password.
+PASSWORD="${RANDOM}"
+echo "${PASSWORD}"
+
+# Three random numbers together.
+PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
+echo "${PASSWORD}"
+
+# Use the current date/time as the basis for the password
+PASSWORD=$(date +%s)
+echo "${PASSWORD}"
+
+# Use nanoseconds to act as randomization.
+PASSWORD=$(date +%s%N)
+echo "${PASSWORD}"
+
+# A better password!
+PASSWORD=$(date +%s%N | sha256sum | head -c32)
+echo "${PASSWORD}"
+
+
