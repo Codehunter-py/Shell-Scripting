896cfdb5c18a9f2184eceea6a8ecbb2252485edf
Codehunter-py <ibrahim.musayev2021@outlook.com>
2021-08-12 23:28:05 +0200

    Redirecting by using File Descriptor

--- luser-demo08.sh
+++ luser-demo08.sh
@@ -22,3 +22,51 @@ echo "${RANDOM} ${RANDOM}" >> ${FILE}
 echo
 echo "Contents of ${FILE}: "
 cat ${FILE}
+
+#################################
+
+# Redirect Standart Input to a program, using File Descriptor 0
+read LINE 0< ${FILE}
+echo
+echo "LINE contains: ${LINE}"
+
+# Redirect Standart Output to a file using File Descriptor 1, overwriting the file
+head -n3 /etc/passwd 1> ${FILE}
+echo
+echo "Contents of ${FILE}"
+cat ${FILE}
+
+# Redirect Standart Error to a file using FD 2 (File Descriptor).
+ERR_FILE="/tmp/data.err"
+head -n3 /etc/passwd /fakefile 2> ${ERR_FILE}
+
+# Redirect Standart Out and Standart Error to a file.
+head -n3 /etc/passwd /fakefile &> ${FILE}
+echo
+echo "Contents of ${FILE}"
+cat ${FILE}
+
+# Redirect Standart Output and Standart Error through a pipe 
+echo
+head -n3 /etc/passwd /fakefile |& cat -n
+
+# Send output to Standart Error
+echo "This is STDERR!" >&2
+
+# Discard Standart Output
+echo
+echo "Discarding STDOUT:"
+head -n3 /etc/passwd /fakefile > /dev/null
+
+# Discard Standart Error
+echo
+echo "Discarding STDERR:"
+head -n3 /etc/passwd /fakefile 2> /dev/null
+
+# Discard STDOUT and STDERR
+echo
+echo "Discarding STDOUT and STDERR"
+head -n3 /etc/passwd /fakefile &> /dev/null
+
+# Clean up
+rm ${FILE} ${ERR_FILE} &> /dev/null
