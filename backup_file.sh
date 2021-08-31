3e8f50ea7e2c6d382d369ae849eafa91ca1b92f4
Codehunter-py <ibrahim.musayev2021@outlook.com>
2021-08-31 22:32:31 +0200

    syslog

--- backup_file.sh
+++ backup_file.sh
@@ -1,10 +1,22 @@
-# Function below creates a backup of a file. Returns non-zero status on error.
+#!/bin/bash
 
+log() {
+  # This function sends a message to syslog and to standard output if VERBOSE is true.
+
+  local MESSAGE="${@}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+    echo "${MESSAGE}"
+  fi
+  logger -t backup_file.sh "${MESSAGE}"
+}
+
+# Function below creates a backup of a file. Returns non-zero status on error.
 backup_file() {
     local FILE="${1}"
-    if [[ -f "{FILE}"]]
+    if [[ -f "${FILE}" ]]
     then
-      local BACKUP_FILE="/var/tmp/$(basename ${FILE}).&(date +%F-%N)"
+      local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
       log "Backing up ${FILE} to ${BACKUP_FILE}"
 
       cp -p ${FILE} ${BACKUP_FILE}
@@ -14,4 +26,14 @@ backup_file() {
     fi
 }  
 
+readonly VERBOSE='true'
 backup_file '/etc/passwd'
+
+# A decision based exit status of the function.
+if [[ "${?}" -eq '0' ]]
+then 
+  log 'File backup succeeded!'
+else
+  log 'File backup failed!'
+  exit 1
+fi
