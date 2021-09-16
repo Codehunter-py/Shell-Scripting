f8bd11353632e59e386fc116b103209105a20d04
Codehunter-py <ibrahim.musayev2021@outlook.com>
2021-09-16 21:58:19 +0200

    script updated

--- del_account.sh
+++ del_account.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# This script deletes a user
+# This script deletes, disables or archives users
+
+readonly ARCHIVE_DIR='/archive'
+usage() {
+    echo "Usage: ${0} [-dra] USER [USERN]..." >&2
+    echo 'Disable a local Linux account.' >&2
+    echo '-d Deletes accounts.' >&2
+    echo '-r Romves the home dir associated w/ the accounts.' >&2
+    echo '-a Creates an archive of the home directory associated with accounts.' >&2
+    
+    exit 1
+}
+
 # run as root
 if [[ "${UID}" -ne 0 ]]
 then 
@@ -8,14 +20,81 @@ echo 'Please run with sudo or as root!' >&2
 exit 1
 fi
 
-USER="$1"
-userdel ${USER}
+while getopts dra OPTION
+do
+  case ${OPTION} in
+    d) DELETE_USER='true';;
+    r) REMOVE_OPTION='-r';;
+    a) ARCHIVE='true';;
+    ?) usage ;;
+  esac
+done
 
-if [[ "${?}" -ne 0 ]]
-then 
-echo "The account ${USER} was already deleted." >&2
-exit 1
+if [[ "${#}" -lt 1 ]]
+then
+usage
 fi
 
-echo "The account ${USER} was deleted."
+for USERNAME in "${@}"
+do
+ echo "Processing user: ${USERNAME}"
+
+ USERID=$(id -u ${USERNAME})
+ if [[ "${USERID}" -lt 1000 ]]
+ then 
+    echo "Refusing to remove the ${USERNAME}" >&2
+    exit 1
+ fi
+
+if [[ "${ARCHIVE}" = 'true' ]]
+then 
+ if [[ ! -d "${ARCHIVE_DIR}" ]]
+ then
+    echo "Creating ${ARCHIVE_DIR} directory."
+    mkdir -p ${ARCHIVE_DIR}
+    if [[ "${?}" -ne 0 ]]
+     then 
+     echo "The archive directory could not be created." >&2
+     exit 1
+    fi
+ fi
+
+ HOME_DIR="/home/${USERNAME}"
+ ARCHIVE_FILE="${ARCHIVE_DIR}/${USERNAME}.tgz"
+ if [[ -d "${HOME_DIR}" ]]
+ then 
+  echo "Archiving ${HOME_DIR} to ${ARCHIVE_FILE}"
+  tar -zcf ${ARCHIVE_FILE} ${HOME_DIR} &> /dev/null
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo "Cannot create ${ARCHIVE_FILE}" >&2
+    exit 1
+  fi
+ else
+  echo "${HOME_DIR} does not exist or is not a dir..." >&2
+  exit 1
+  fi
+ fi
+
+ if [[ "${DELETE_USER}" = 'true' ]] 
+ then
+ # Delete a user 
+ userdel ${REMOVE_OPTION} ${USERNAME}
+ # CHECK if it was successful
+  if [[ "${?}" -ne 0 ]]
+  then 
+    echo "The account ${USERNAME} was not deleted" >&2
+    exit 1
+  fi
+  echo "The accout ${USERNAME} was deleted."
+ else
+   chage -E 0 ${USERNAME}
+ if [[ "${?}" -ne 0 ]]
+ then 
+   echo "The account ${USERNAME} was already deleted." >&2
+   exit 1
+ fi
+ echo "The account ${USERNAME} was disabled."
+ fi
+done
 exit 0
