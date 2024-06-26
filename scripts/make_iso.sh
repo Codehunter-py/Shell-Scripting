d84627049281f13083d8165f4326d863e94d72db
Ibrahim Musayev <84103775+Codehunter-py@users.noreply.github.com>
2024-06-26 15:47:28 +0200

    include options to choose between different version of ks files

--- scripts/make_iso.sh
+++ scripts/make_iso.sh
@@ -3,15 +3,15 @@
 ################################################################################
 # Name    : make_iso.sh                                                        #
 # Author  : Ibrahim Musayev                                                    #
-# Purpose : creates a new ISO image named Rocky-8-x86_64.iso and includes     #
+# Purpose : creates a new ISO image named Rocky-9-x86_64.iso and includes     #
 #           latest version of kickstart script. Also, The script provides      # 
 #           an interactive menu for the user to choose between generating      #
 #           a new kickstart script or proceeding with an existing one.         #
 # History : 15.05.23 Ibrahim Musayev, creation                                 #
 ################################################################################
 
-iso_file="Rocky-8.9-x86_64-minimal.iso"
-new_iso_name="Rocky-8-9-x86_64-dvd.iso"
+iso_file="Rocky-9.4-x86_64-minimal.iso"
+new_iso_name="Rocky-9-4-x86_64-dvd.iso"
 downloads_dir="$HOME/Downloads"
 new_iso_dir=$downloads_dir/iso
 mnt_iso_dir="/mnt/iso"
@@ -19,7 +19,7 @@ tmp_workdir="/tmp/workdir"
 tmp_workdir_iso="$tmp_workdir/iso"
 kickstart_dir="$tmp_workdir_iso/kickstart"
 python_replace_parameters="replace_parameters_in_ks_cfg.py"
-ks_cfg="iad1-ks-physical.cfg"
+ks_cfg="01_ams1-ks-physical-rocky-installation.cfg" #default
 isolinux_cfg="isolinux-rocky.cfg"
 user_mod=$(whoami)
 
@@ -50,9 +50,38 @@ proceed_with_existing_kickstart_script() {
     else
         read -p "The kickstart script is currently set to '$ks_cfg'. Are you sure you want to continue with this script? (y/n): " confirm
         if [ "$confirm" != "y" ]; then
-            read -p "Please provide a new name for the kickstart script: " ks_cfg
+            echo "Please choose a kickstart script:"
+            echo "1) 01_ams1-ks-physical-rocky-installation.cfg"
+            echo "2) 02_fra1-ks-physical-rocky-installation.cfg"
+            echo "3) 03_west1-ks-physical-rocky-installation.cfg"
+            echo "4) 04_east1-ks-physical-rocky-installation.cfg"
+            echo "5) Enter custom name"
+            read -p "Enter your choice (1-5): " choice
+
+            case $choice in
+                1)
+                    ks_cfg="01_ams1-ks-physical-rocky-installation.cfg"
+                    ;;
+                2)
+                    ks_cfg="02_fra1-ks-physical-rocky-installation.cfg"
+                    ;;
+                3)
+                    ks_cfg="03_west1-ks-physical-rocky-installation.cfg"
+                    ;;
+                4)
+                    ks_cfg="04_east1-ks-physical-rocky-installation.cfg"
+                    ;;
+                5)
+                    read -p "Please provide a new name for the kickstart script: " ks_cfg
+                    ;;
+                *)
+                    echo "Invalid choice. Please run the script again."
+                    exit 1
+                    ;;
+            esac
         fi
     fi
+    echo "Using kickstart script: $ks_cfg"
 }
 
 detect_python_command
@@ -120,7 +149,7 @@ if [ -f "$downloads_dir/$iso_file" ]; then
       if [ $? -eq 0 ]; then
           echo "Kickstart script copied successfully."
           echo "Creating a $new_iso_name File" && cd $tmp_workdir_iso
-          sudo genisoimage -o $new_iso_name -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "Rocky 8 x86_64" -R -J -v -T .
+          sudo genisoimage -o $new_iso_name -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "Rocky-9-x86_64" -R -J -v -T .
           if [ $? -eq 0 ]; then
               echo "$new_iso_name File successfully created..."
               sudo mv $new_iso_name $new_iso_dir
@@ -142,6 +171,6 @@ if [ -f "$downloads_dir/$iso_file" ]; then
     fi
 else
     echo "Please download the ISO file: $iso_file"
-    echo "First you need a Rocky-8 ISO --> https://rockylinux.org/download/"
+    echo "First you need a Rocky-9 ISO --> https://rockylinux.org/download/"
     exit 1
 fi
